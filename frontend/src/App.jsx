cf91248a1812cc760fb63fe32cd5df4f7b0e023a
Harshit Raj <harshitraj32257@gmail.com>
2026-05-08 02:15:30 +0530

    new add pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,8 @@ import ManageTasks from "./pages/Admin/Managetask"
 import ManageUsers from "./pages/Admin/Manageuser"
 import CreateTask from "./pages/Admin/Createtask"
 
-// You will need to create this PrivateRoute component if you haven't already!
-import PrivateRoute from "./components/PrivateRoute" 
+// Corrected import path for PrivateRoute!
+import PrivateRoute from "./routes/PrivateRoute" 
 import UserDashboard from './pages/users/userDashboard'
 import MyTask from './pages/users/MyTask'
 import TaskDetails from './pages/users/TaskDetails'
@@ -29,19 +29,19 @@ const App = () => {
           <Route path="/signup" element={<Signup />} />
 
           {/* Admin Protected Routes */}
-          {/* Note: Comment out the PrivateRoute wrapper for a second if you haven't built it yet, 
-              or the app will crash because PrivateRoute doesn't exist! */}
           <Route element={<PrivateRoute allowedRoles={['admin']} />}> 
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/admin/tasks" element={<ManageTasks />} />
             <Route path="/admin/users" element={<ManageUsers />} />
             <Route path="/admin/create-task" element={<CreateTask />} />
           </Route> 
+          
+          {/* User Protected Routes */}
           <Route element={<PrivateRoute allowedRoles={['user']}/>}>
-          <Route path="/user/dashboard" element={<UserDashboard/>}/>
-          <Route path="/user/tasks" element={<MyTask/>}/>
-          <Route path="/user/task-details/:id" element={<TaskDetails/>}/>
-        </Route>
+            <Route path="/user/dashboard" element={<UserDashboard/>}/>
+            <Route path="/user/tasks" element={<MyTask/>}/>
+            <Route path="/user/task-details/:id" element={<TaskDetails/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
-const privateroute = ({allowedRoles}) => {
-  return (
-    <outlet/>
-  )
-}
+const PrivateRoute = ({ allowedRoles }) => {
+  // Replace this with your actual authentication logic. 
+  // For now, we'll try to get the user from localStorage.
+  const user = JSON.parse(localStorage.getItem('user')); 
 
-export default privateroute
+  // If there is no user logged in at all, redirect to login
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // If roles are specified, check if the user has the required role
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    // If they don't have permission, maybe send them back to their specific dashboard
+    return <Navigate to="/login" replace />; 
+  }
+
+  // If authenticated and authorized, render the child routes
+  return <Outlet />;
+};
+
+export default PrivateRoute;
