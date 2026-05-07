ba29567c334b8ec6a6ae528c670a17b17024774e
Harshit Raj <harshitraj32257@gmail.com>
2026-05-07 17:52:28 +0530

    tailwind css installation and setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>task manager</div>
-  )
+  // Notice the capital 'C' in className!
+  return <div className="text-red-500 text-3xl">task </div>
 }
 
 export default App
