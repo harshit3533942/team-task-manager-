febb3235fb28ec54ca571c4b0b951185d8adce83
Harshit Raj <harshitraj32257@gmail.com>
2026-05-26 18:32:05 +0530

    update task api route

--- backened/controller/task.controller.js
+++ backened/controller/task.controller.js
@@ -98,3 +98,29 @@ export const getTaskById = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateTask = async (req, res, next) => {
+    try{
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return next(errorHandler(404, "Task not found"));
+} 
+      task.title = req.body.title || task.title;
+        task.description = req.body.description || task.description;
+        task.priority = req.body.priority || task.priority;
+        task.dueDate = req.body.dueDate || task.dueDate;
+        task.todoChecklist = req.body.todoChecklist || task.todoChecklist;
+        task.attachment = req.body.attachment || task.attachment;
+
+        if (req.body.assignedTo) {
+            if (!Array.isArray(req.body.assignedTo)) {
+                return next(errorHandler(400, "assignedTo must be an array of user IDs"));
+            }
+            task.assignedTo = req.body.assignedTo;
+        }
+        const updatedTask = await task.save();
+        return res.status(200).json({ message: "Task updated successfully", task: updatedTask });
+}catch(error){
+        next(error);
+    }
+};
