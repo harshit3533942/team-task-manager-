9bbea808ed5a46f0f8244fdfe9aeca8021be42eb
Harshit Raj <harshitraj32257@gmail.com>
2026-05-26 10:31:49 +0530

    get user by id api route

--- backened/controller/user.controller.js
+++ backened/controller/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Task from "../models/task.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const getUsers = async (req, res, next) => {
   try {
@@ -42,4 +43,16 @@ export const getUsers = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const getUserById = async (req, res, next) => {
+try {
+  const userId = await User.findById(req.params.id).select("-password");
+  if (!userId) {
+    return next(errorHandler(404, "User not found"));
+}
+  res.status(200).json(userId);
+} catch (error) {
+  next(error);
+}
 };
