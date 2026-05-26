892448a0f7e97e73b5c6708bd27322241cba241d
Harshit Raj <harshitraj32257@gmail.com>
2026-05-26 18:13:34 +0530

    get task by id api route

--- backened/routes/task.route.js
+++ backened/routes/task.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { adminOnly, verifyToken } from "../utils/verifyUser.js";
-import { createTask } from "../controller/task.controller.js";
+import { createTask, getTasks } from "../controller/task.controller.js";
 
 const router = express.Router();
 
 router.post("/create",verifyToken, adminOnly, createTask);
+
+router.get("/", verifyToken, getTasks);
+
+router.get("/:id", verifyToken, getTasks);
 
 export default router;
